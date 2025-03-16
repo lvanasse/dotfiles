ce9730e08f6c60040c73c0094da743ffe0d40a6d
Ludovic Vanasse <mail@ludovicvanasse.com>
2025-03-15 22:46:17 -0400

    fix hm and add determinate nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,78 @@
+# {
+#   description = "Ludovic Vanasse NixOS's flake";
+
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+#     home-manager.url = "github:nix-community/home-manager/release-24.11";
+#     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+#   };
+
+#   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+#     nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
+#       system = "x86_64-linux";
+#       modules = [
+#         ./configuration.nix
+
+#         home-manager.nixosModules.home-manager {
+#             home-manager.useGlobalPkgs = true;
+#             home-manager.useUserPackages = true;
+#             home-manager.users.ludovic = import ./home.nix;
+#             home-manager.backupFileExtension = "backup";
+#         }
+#       ];
+#     };
+#   };
+# }
+
+
 {
-  description = "Ludovic Vanasse NixOS's flake";
+  description = "NixOS and Home Manager Configuration";
 
   inputs = {
+    determinate.url = "github:DeterminateSystems/determinate";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, determinate, ... }:
+    let
       system = "x86_64-linux";
+      username = "ludovic";
+      hostname = "pc";
+    in {
+      nixosConfigurations = {
+        ${hostname} = nixpkgs.lib.nixosSystem {
+          inherit system;
       modules = [
         ./configuration.nix
-
-        home-manager.nixosModules.home-manager {
+            home-manager.nixosModules.home-manager
+            {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.ludovic = import ./home.nix;
-            home-manager.backupFileExtension = "backup";
+              home-manager.users.${username} = import ./home.nix;
+            }
+            determinate.nixosModules.default
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        "${username}@${hostname}" = home-manager.lib.homeManagerConfiguration {
+          inherit system;
+          pkgs = import nixpkgs { inherit system; };
+          modules = [
+            ./home.nix
+            {
+              home.username = username;
+              home.homeDirectory = "/home/${username}";
+              home.stateVersion = "24.11"; # Adjust to your Home Manager version
         }
+            determinate.nixosModules.default
       ];
+        };
     };
   };
 }

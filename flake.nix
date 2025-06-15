7c2182eee99eea8626e916eb64db6ecafdf6afa2
Ludovic Vanasse <lvanasse@luxaerobot.com>
2025-06-15 11:53:32 -0400

    flake.nix: Remove old commented config

--- flake.nix
+++ flake.nix
@@ -1,29 +1,3 @@
-# {
-#   description = "Ludovic Vanasse NixOS's flake";
-
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-#     home-manager.url = "github:nix-community/home-manager/release-24.11";
-#     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-#   };
-
-#   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-#     nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
-#       system = "x86_64-linux";
-#       modules = [
-#         ./configuration.nix
-
-#         home-manager.nixosModules.home-manager {
-#             home-manager.useGlobalPkgs = true;
-#             home-manager.useUserPackages = true;
-#             home-manager.users.ludovic = import ./home.nix;
-#             home-manager.backupFileExtension = "backup";
-#         }
-#       ];
-#     };
-#   };
-# }
-
 {
   description = "NixOS and Home Manager Configuration";
 

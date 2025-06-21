fa1eb72c16a171295489313649c035da2e5a7575
Ludovic Vanasse <lvanasse@luxaerobot.com>
2025-06-20 22:25:40 -0400

    flake.nix: Add qbittorrent pin for v5.0.5

--- flake.nix
+++ flake.nix
@@ -2,9 +2,16 @@
   description = "NixOS and Home Manager Configuration";
 
   inputs = {
-    determinate.url = "github:DeterminateSystems/determinate";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    determinate.url = "github:DeterminateSystems/determinate";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -13,45 +20,77 @@
       nixpkgs,
       home-manager,
       determinate,
+      plasma-manager,
       ...
     }:
     let
       system = "x86_64-linux";
       username = "ludovic";
       hostname = "pc";
+
+      qbittorrent505 = final: prev: {
+        qbittorrent = prev.qbittorrent.overrideAttrs (_: {
+          version = "5.0.5";
+          src = prev.fetchurl {
+            url = "https://github.com/qbittorrent/qBittorrent/archive/refs/tags/release-5.0.5.tar.gz";
+            sha256 = "sha256-ebAwVl+jkqa8Jnsk9TjXuOdi9gfuc0s9RZsZxhwWi3M=";
+          };
+        });
+      };
     in
     {
-      nixosConfigurations = {
-        ${hostname} = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
+      overlays.default = qbittorrent505;
+
+      nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs username hostname; };
+
+        modules = [
+          ./configuration.nix
+          determinate.nixosModules.default
+
+          (
+            { config, ... }:
             {
-              home-manager.useGlobalPkgs = true;
+              nixpkgs.overlays = [ qbittorrent505 ];
+            }
+          )
+
+          home-manager.nixosModules.home-manager
+          (
+            { pkgs, ... }:
+            {
               home-manager.useUserPackages = true;
               home-manager.users.${username} = import ./home.nix;
+
+              home-manager.sharedModules = [
+                plasma-manager.homeManagerModules.plasma-manager
+                (
+                  { config, ... }:
+                  {
+                    nixpkgs.overlays = [ qbittorrent505 ];
+                  }
+                )
+              ];
+
+              nixpkgs.overlays = [ qbittorrent505 ];
             }
-            determinate.nixosModules.default
-          ];
-        };
+          )
+        ];
       };
 
-      homeConfigurations = {
-        "${username}@${hostname}" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."${username}@${hostname}" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
           inherit system;
-          pkgs = import nixpkgs { inherit system; };
-
-          username = username;
-          homeDirectory = "/home/${username}";
-          stateVersion = "25.05";
-
-          modules = [
-            ./home.nix
-
-            determinate.nixosModules.default
-          ];
+          overlays = [ qbittorrent505 ];
         };
+
+        extraSpecialArgs = { inherit inputs username hostname; };
+
+        modules = [
+          ./home.nix
+          plasma-manager.homeManagerModules.plasma-manager
+        ];
       };
     };
 }

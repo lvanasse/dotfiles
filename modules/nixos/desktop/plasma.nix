ca7c662a0cac98e64f88b4da667b9512099809dd
Ludovic Vanasse <mail@ludovicvanasse.com>
2025-10-19 12:50:21 -0400

    modules: Configure KDE portals (prefer kde;gtk)

--- modules/nixos/desktop/plasma.nix
+++ modules/nixos/desktop/plasma.nix
@@ -1,5 +1,5 @@
 # KDE Plasma desktop environment
-{ ... }:
+{ pkgs, lib, ... }:
 {
   # Display manager
   services.displayManager.sddm = {
@@ -12,4 +12,15 @@
   services.desktopManager.plasma6.enable = true;
 
   # Additional desktop packages (none specific)
+
+  # Enable XDG desktop portals suitable for Plasma sessions
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      kdePackages.xdg-desktop-portal-kde
+      xdg-desktop-portal-gtk
+    ];
+    # Prefer KDE portal when running in Plasma sessions, fall back to GTK
+    config.kde.default = lib.mkForce "kde;gtk";
+  };
 }

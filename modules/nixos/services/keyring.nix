63c52121d7683edd7e8a4358f4d468beaced2bd6
Ludovic Vanasse <mail@ludovicvanasse.com>
2025-10-26 12:51:41 -0400

    modules: Keyring: enable GNOME Keyring unlock via PAM

--- modules/nixos/services/keyring.nix
+++ modules/nixos/services/keyring.nix
@@ -1,4 +1,12 @@
 # Keyring and credential management
 _: {
+  # Provide Secret Service (used by Slack, etc.)
   services.gnome.gnome-keyring.enable = true;
+
+  # Ensure the keyring is unlocked on login via PAM (SDDM)
+  security.pam.services.sddm.enableGnomeKeyring = true;
+  # Also enable for TTY logins, just in case
+  security.pam.services.login.enableGnomeKeyring = true;
+  # Unlock on screen unlock
+  security.pam.services.swaylock.enableGnomeKeyring = true;
 }

b084024a0f808d3d857cefc48fe6ec4056736e00
Ludovic Vanasse <mail@ludovicvanasse.com>
2025-10-26 23:10:28 -0400

    home: Manage Firefox via HM and set SearXNG
    - Move Firefox from package list to Home Manager-managed program\n- Enforce SearXNG as default (and private) search engine\n- Disable search suggestions; add aliases (@sx, @searx)\n\nTested:\n- Built HM activation for pc and laptop\n- Built NixOS toplevel for pc and laptop

--- modules/home/packages/desktop.nix
+++ modules/home/packages/desktop.nix
@@ -3,7 +3,6 @@
 {
   home.packages = with pkgs; [
     # Web browsers
-    firefox
     ungoogled-chromium
 
     # Communication
--- modules/home/programs/firefox.nix
+++ modules/home/programs/firefox.nix
@@ -1,6 +1,25 @@
 _: {
   programs.firefox = {
     enable = true;
+    # Enforce default search engine at the browser level for all profiles
+    policies = {
+      DefaultSearchEngine = "SearXNG";
+      DefaultSearchEnginePrivate = "SearXNG";
+      SearchSuggestEnabled = false;
+      SearchEngines = {
+        Add = [
+          {
+            Name = "SearXNG";
+            URLTemplate = "https://search.rhscz.eu/search?q={searchTerms}";
+            IconURL = "https://search.rhscz.eu/favicon.ico";
+            Aliases = [
+              "@sx"
+              "@searx"
+            ];
+          }
+        ];
+      };
+    };
     profiles = {
       default = {
         isDefault = true;

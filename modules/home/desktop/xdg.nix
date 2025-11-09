bdd926ac92a61643b0aaa3e2d5cfa3a9048232a8
Ludovic Vanasse <mail@ludovicvanasse.com>
2025-11-09 14:49:44 -0500

    home: Set Firefox as default browser; force mimeapps
    Define handlers for http/https/html to firefox.desktop and enable xdg.configFile.mimeapps.list.force.

--- modules/home/desktop/xdg.nix
+++ modules/home/desktop/xdg.nix
@@ -7,6 +7,23 @@
       "inode/directory" = [ "org.kde.dolphin.desktop" ];
       "application/x-directory" = [ "org.kde.dolphin.desktop" ];
       "x-scheme-handler/trash" = [ "org.kde.dolphin.desktop" ];
+      # Force Firefox as the handler for generic web content
+      "text/html" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+      "x-scheme-handler/slack" = [ "slack.desktop" ];
     };
   };
+
+  # Allow Home Manager to replace an existing ~/.config/mimeapps.list
+  xdg.configFile."mimeapps.list".force = true;
 }

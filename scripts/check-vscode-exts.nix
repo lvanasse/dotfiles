3b45cfc87d29f6068680a7981539924269c4c455
Ludovic Vanasse <mail@ludovicvanasse.com>
2025-11-19 09:38:15 -0500

    modules: Add VS Code marketplace publishers check script
    Add Nix script to verify availability of selected publishers in nix-vscode-extensions overlay.

---
+++ scripts/check-vscode-exts.nix
@@ -0,0 +1,32 @@
+let
+  flake = builtins.getFlake (toString ./.);
+  pkgs = import flake.inputs.nixpkgs {
+    system = "x86_64-linux";
+    overlays = [ flake.inputs.nix-vscode-extensions.overlays.default ];
+    config.allowUnfree = true;
+  };
+  publishers = [
+    "openai"
+    "mcu-debug"
+    "jebbs"
+    "mkhl"
+    "jeff-hykin"
+    "rust-lang"
+    "ms-vscode"
+    "jdinhlife"
+    "jnoortheen"
+    "bbenoist"
+    "vscode-icons-team"
+    "streetsidesoftware"
+    "redhat"
+    "foxundermoon"
+    "marus25"
+    "james-yu"
+  ];
+in
+builtins.listToAttrs (
+  map (p: {
+    name = p;
+    value = builtins.hasAttr p pkgs.vscode-marketplace;
+  }) publishers
+)

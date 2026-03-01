b1a4e109c0bec5bb3a3880a97a5c71f20993de5c
Ludovic Vanasse <mail@ludovicvanasse.com>
2026-03-01 17:50:40 -0500

    chore(server): harden network and firewall

--- modules/nixos/target/server.nix
+++ modules/nixos/target/server.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   flake.modules.nixos."targetConfig.server" =
     { inputs, username, pkgs, ... }:
@@ -19,7 +19,29 @@
         "time3.google.com"
         "time4.google.com"
       ];
-      networking.nameservers = [ "1.1.1.1" ];
+      networking = {
+        nameservers = [ "1.1.1.1" ];
+
+        # Server network is managed by systemd-networkd with static LAN address.
+        useNetworkd = true;
+        networkmanager.enable = lib.mkForce false;
+        useDHCP = false;
+
+        interfaces.enp1s0 = {
+          useDHCP = false;
+          ipv4.addresses = [
+            {
+              address = "192.168.0.50";
+              prefixLength = 24;
+            }
+          ];
+        };
+
+        defaultGateway = {
+          address = "192.168.0.1";
+          interface = "enp1s0";
+        };
+      };
 
       # Headless server settings
       boot.loader.systemd-boot.enable = true;
@@ -31,21 +53,29 @@
       # Basic server packages
       environment.systemPackages = with pkgs; [
         htop
+        btop
         iotop
         ncdu
         git
         curl
         wget
         rsync
+        codex
       ];
 
       # Enable fstrim for SSD
       services.fstrim.enable = true;
 
-      # Firewall - trust tailscale
+      # Firewall - trust tailscale and allow LAN access on enp1s0
       networking.firewall = {
         enable = true;
-        trustedInterfaces = [ "tailscale0" ];
+        trustedInterfaces = [ "tailscale0" "docker0" ];
+        allowedTCPPorts = lib.mkForce [ ];
+        allowedUDPPorts = lib.mkForce [ ];
+        interfaces.enp1s0.allowedTCPPorts = lib.mkForce [ 22 ];
+        interfaces.enp1s0.allowedUDPPorts = lib.mkForce [ ];
       };
+
+      services.openssh.openFirewall = lib.mkForce false;
     };
 }

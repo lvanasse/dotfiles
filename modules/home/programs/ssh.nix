872169d439f27ce340c86d519b371a2339f51a68
Ludovic Vanasse <mail@ludovicvanasse.com>
2026-07-27 11:08:34 -0400

    ssh: add vention password host

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -122,6 +122,12 @@ in
             identityAgent = "none";
           };
 
+          "vention 192.168.7.2" = {
+            hostname = "192.168.7.2";
+            user = "vention";
+            pubkeyAuthentication = false;
+          };
+
           # Steam Deck (SteamOS)
           "steamdeck" = {
             hostname = "192.168.0.105";
